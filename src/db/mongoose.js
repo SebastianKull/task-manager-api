c62cd7784ed1be1cbba7c3dafe43164c3821d988
sebastiankull <s.kull@iftra.de>
2019-07-09 20:32:15 +0200

    init

---
+++ src/db/mongoose.js
@@ -0,0 +1,7 @@
+const mongoose = require('mongoose')
+
+mongoose.connect('mongodb://0.0.0.0:27017/task-manager-api', {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+})
