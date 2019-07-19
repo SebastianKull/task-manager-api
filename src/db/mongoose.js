4d3cafea4ee6aa7a420bce8cfc1f8420f2982d12
Sebastian <sebastianKull@web.de>
2019-07-20 00:04:05 +0200

    Config für Live-Datenbank

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,6 +1,17 @@
 const mongoose = require('mongoose')
+require('dotenv').config()
 
-mongoose.connect('mongodb://0.0.0.0:27017/task-manager-api', {
+var connection = 'mongodb://0.0.0.0:27017'
+const dbname = process.env.DB_NAME || 'task-manager-api'
+
+if(process.env.PRODUCTION === "true") {
+    const user = process.env.DB_USER
+    const pass = process.env.DB_PASS
+    const host = process.env.DB_HOST || connection
+    connection = `mongodb+srv://${user}:${pass}@${host}`
+}
+
+mongoose.connect(`${connection}/${dbname}`, {
     useNewUrlParser: true,
     useCreateIndex: true,
     useFindAndModify: false
